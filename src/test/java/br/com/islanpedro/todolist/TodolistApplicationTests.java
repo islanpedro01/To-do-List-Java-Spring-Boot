41e1550d4290f4c23fa9a8a85723fd5dec70bbad
islanpedro01 <islanpedro.12@hotmail.com>
2023-10-15 00:55:42 -0300

    Finishing Java Spring boot project

---
+++ src/test/java/br/com/islanpedro/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.islanpedro.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
